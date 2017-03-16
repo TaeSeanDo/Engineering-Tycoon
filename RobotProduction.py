8bc6a1811c5d93e01e698cb14562e4dd51c2e97b
TaeSeanDo <sean.transducer3000@gmail.com>
2017-03-16 08:20:25 -0700

    starting on string method of robot production class

--- RobotProduction.py
+++ RobotProduction.py
@@ -5,10 +5,10 @@ Robot-production class
 
 class Robot:
     """docstring for Robot"""
-    def __init__(self, budget, colors, size, max_speed = 1, cooling_system = 1,  battery_life, consumer_utility = 0, revenue = 0):
+    def __init__(self, budget, colors, size, max_speed = 1, cooling_system = 1,  battery_life = 1, consumer_utility = 0, revenue = 0):
         super(Robot, self).__init__()
         self.budget = budget
-        self.color = colors
+        self.colors = colors
         self.size =  size
         self.max_speed = max_speed
         self.torque = torque
@@ -20,4 +20,11 @@ class Robot:
         self.consumer_utility += consumer_utility_change
         self.revenue += revenue_change
         self.budget += budget_change
-    def __str__(self)
+    def __str__(self):
+        msg = "Your robot is " + size[0] + " x " size[1] " x " + size[2] " and comes in "
+        for i in range self.colors.length:
+            msg += self.colors[i]
+            if i < self.colors.length - 1:
+                msg += ", "
+            if i == self.colors.length - 2:
+                msg += "and "
