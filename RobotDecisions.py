e8aa74759c2102cf58eff023b1b0f31da334a8f1
TaeSeanDo <sean.transducer3000@gmail.com>
2017-03-22 16:44:03 -0700

    template for decision algorithm

--- RobotDecisions.py
+++ RobotDecisions.py
@@ -14,4 +14,29 @@ def robot_code():
     robot = Robot(difficulty, colors, size)
     print robot
 
-    print "The best way to earn a profit is to invest in your product. Would you like to invest in (1) the speed of your robot, (2) your cooling system, or (3) the battery life?"
+
+
+
+    decision_num = 10
+    for i in range decision_num:
+        if i == 0:
+            try:
+                decision1 = int(raw_input("The best way to earn a profit is to invest in your product. Would you like to invest in (1) the speed of your robot, (2) your cooling system, or (3) the battery life?"))
+            except:
+                decision1 = int(raw_input("Put a number, please."))
+            if decision1 == 1:
+                max_speed_increase = 1
+                cooling_increase = 0
+                battery_life_increase = 0
+                cost = 2000
+            elif decision1 == 2:
+                max_speed_increase = 0
+                cooling_increase = 1
+                battery_life_increase = 0
+                cost = 2000
+            elif decision1 == 3:
+                max_speed_increase = 0
+                cooling_increase = 0
+                battery_life_increase = 1
+                cost = 5000
+            robot.investment(max_speed_increase, cooling_increase, battery_life_increase, cost)
