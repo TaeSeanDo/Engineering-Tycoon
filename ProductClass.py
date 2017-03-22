3ee017601de032080f70a5208d76bf4b97990ede
TaeSeanDo <sean.transducer3000@gmail.com>
2017-03-21 18:13:11 -0700

    product class

---
+++ ProductClass.py
@@ -0,0 +1,20 @@
+
+"""
+Sean Tierney
+Product class
+"""
+
+class Product:
+    """docstring for Robot"""
+    def __init__(self, difficulty, consumer_utility, revenue):
+        self.budget = 600 / difficulty
+        self.consumer_utility = consumer_utility
+        self.revenue = revenue
+    def investment(self, consumer_utility_change, revenue_change, budget_change):
+        self.consumer_utility += consumer_utility_change
+        self.revenue += revenue_change
+        self.budget += budget_change
+    def get_budget(self):
+        msg = "Your budget is "
+        msg += str(self.budget)
+        return msg
