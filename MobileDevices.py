e95a015169c054d85c56b1513cadc330d3a476ac
Arnob1 <arnobkabir2022@gmail.com>
2017-03-26 15:29:59 -0700

    Revert "commity thingy"
    This reverts commit 7173ad65a4a758c94d6febeeb0824393f0897ad1.

--- MobileDevices.py
+++ MobileDevices.py
@@ -1,36 +1,39 @@
 #Sets up the class for mobile devices
-from math import log10
 from ProductClass import Product
 class mobile_devices(Product):
-    def __init__(self, difficulty, colors, headjack_YN, battery_life_MD = 1, material, processor_proficiency = 1, internal_storage_capacity = 1, revenue = 0):
-        self.difficulty
+    def __init__(self, difficulty, colors, headjack_YN, battery_life, material,
+        processor_proficiency = 1, internal_storage_capacity = 1, revenue = 0):
         self.colors = colors
         self.headjack_YN = headjack_YN
-        self.battery_life_MD = battery_life_MD
+        self.battery_life = battery_life
         self.material = material
 
 #Investment function keeps track of money
-    def investment(self, internal_storage_capacity_increase, processor_proficiency_increase, battery_life_MD_increase, cost):
-        self.internal_storage_capacity += internal_storage_capacity_increase
-        self.processor_proficiency += processor_proficiency_increase
-        self.battery_life_MD += battery_life_MD_increase
-        revenue = 3000 * log10(self.internal_storage_capacity) + 1800 * log10(self.processor_proficiency) + 4800 * log10(self.battery_life_MD)
+    def investment(self, revenue_change, budget_change):
+        self.revenue += revenue_change
+        self.budget += budget_change
 
-    def __str__(self):
-        msg = "Your  mobile device comes in " + str(self.colors) + " colors. It "
-
-"""
-Sean Tierney
-Robot-production class
-"""
-from math import log10
+from math import pow
 from ProductClass import Product
 
+class Robot(Product):
+    """Class for Robot, inherits Product class"""
+    def __init__(self, difficulty, colors, size, max_speed = 1, cooling_system = 1,  battery_life = 1, revenue = 0):
+        Product.__init__(self, difficulty, revenue)
+        self.colors = colors
+        self.size =  size
+        self.max_speed = max_speed
+        self.cooling_system = cooling_system
+        self.battery_life = battery_life
+    def investment(self, max_speed_increase, cooling_system_increase, battery_life_increase, cost):
+        self.max_speed += max_speed_increase
+        self.cooling_system += cooling_system_increase
+        self.battery_life += battery_life_increase
+        revenue = 5000 * pow(self.max_speed, 1.0/3) + 3000 * pow(self.cooling_system, 1.0/3) + 8000 * pow(self.battery_life, 1.0/3)
+        Product.investment(self, revenue, cost)
     def __str__(self):
-        msg = "Your robot is " + str(self.size[0]) + " x " + str(self.size[1]) + " x " + str(self.size[2]) + " and comes in " + str(len(self.colors)) + " color"
-        if len(self.colors) != 1:
-            msg += 's'
-        msg += ': '
+        msg = "Your robot is " + str(self.size[0]) + " x " + str(self.size[1]) + " x " + str(self.size[2]) \
+                + " and comes in " + str(len(self.colors)) + " colors: "
         for i in range (len(self.colors)):
             msg += self.colors[i]
             if len(self.colors) == 2:
@@ -41,10 +44,6 @@ from ProductClass import Product
                     msg += ", "
                 if i == len(self.colors) - 2:
                     msg += "and "
-        msg += ". \n\n\tMaximum speed in meters/sec: " + str(self.max_speed)
-        msg += "\n\tCooling system proficiency: " + str(self.cooling_system)
-        msg += "\n\tBattery life in hours: " + str(self.battery_life)
-        msg += "\n\n\tDifficulty: " + str(self.difficulty)
-        msg += "\n\tBudget: " + str(self.budget)
-        msg += "\n\tRevenue: " + str(self.revenue) + '\n'
+        msg += ". \n\tBudget: " + str(self.budget)
+        msg += "\n\tRevenue: " + str(self.revenue)
         return msg
